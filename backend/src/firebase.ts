9078c7b7721adae3da2493a6152996d11deeddd8
Julian Mayes <julianmayes@gmail.com>
2025-01-16 14:22:52 -0800

    env setup

--- backend/src/firebase.ts
+++ backend/src/firebase.ts
@@ -3,12 +3,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import * as admin from "firebase-admin";
 
-// Load your service account key JSON
-import * as serviceAccount from "../sonatrio-firebase-adminsdk-5ytkw-c8d13500ab.json";
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(
+            require(process.env.FIREBASE_ADMIN_KEY_PATH!)
+        ),
+    });
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+    console.log("Firebase Admin initialized successfully.");
+} catch (error) {
+    if (error instanceof Error) {
+        console.error("Error message:", error.message);
+    } else {
+        console.error("Unknown error:", error);
+    }
+}
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,13 +29,13 @@ admin.initializeApp({
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB5EgLxYFYs5UoQSA4eyprACSNAE2D2_a8",
-    authDomain: "sonatrio.firebaseapp.com",
-    projectId: "sonatrio",
-    storageBucket: "sonatrio.firebasestorage.app",
-    messagingSenderId: "314891765825",
-    appId: "1:314891765825:web:fecec00997339f33b39ec0",
-    measurementId: "G-SLWKFL1Q8P",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
